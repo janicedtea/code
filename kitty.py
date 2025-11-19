830fe5425c184c275af77799e94131f1a4bd69c5
janicedtea <janicesu2019@gmail.com>
2025-11-18 20:41:48 -0500

    add player to screen (scaled) with coordinates

--- kitty.py
+++ kitty.py
@@ -9,10 +9,15 @@ gray = (128, 128, 128)
 width = 400
 height = 500
 background = white
-
-player = pygame.image.load('kittyplayer.jpg')
+player = pygame.transform.scale(pygame.image.load ('kittyplayer.jpg'), (90, 70))
 fps = 60
 timer = pygame.time.Clock()
+
+
+#game variables
+player_x = 170
+player_y = 400
+
 
 #screen
 screen = pygame.display.set_mode([width, height])
@@ -22,6 +27,8 @@ running = True
 while running == True:
     timer.tick(fps)
     screen.fill(background)
+    screen.blit(player, (player_x, player_y))
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
